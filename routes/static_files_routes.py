7abd66f353a08f2561f37871eed7be3be817b5bf
Ghaida Al Farsi <ghaida.farsi1@omantel.om>
2024-09-25 09:36:26 +0400

    adding files (#6)
    Co-authored-by: Ghaida Adil Al Farsi <71589@omantel.om>

--- routes/static_files_routes.py
+++ routes/static_files_routes.py
@@ -3,7 +3,17 @@ from fastapi.responses import FileResponse
 
 static_files_router = APIRouter()
 
-
+# Route for index.html
 @static_files_router.get("/")
 async def homepage():
     return FileResponse("static/index.html")
+
+# Route for ContextMail.html
+@static_files_router.get("/ContextMail")
+async def context_mail():
+    return FileResponse("static/ContextMail.html")
+
+
+
+
+
